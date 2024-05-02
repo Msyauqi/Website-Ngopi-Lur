5fe72a58c5919dfbd4c1aae9fdef8cde566ff96e
Cayy78 <rafficahya08@gmail.com>
2024-05-02 18:06:13 +0700

    push yang sebelumnya

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,3 +1,35 @@
-function goToPagemenu() {
-    window.location.href = "/home";
-  }
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
+const sb = document.querySelector('#search-button');
+
+document.addEventListener('click', function (e){
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
+});
+
+function searchMenu() {
+    // Mendapatkan nilai input pencarian
+    var input = document.getElementById('search-box').value.toUpperCase();
+
+    // Mendapatkan semua menu cards
+    var menuCards = document.querySelectorAll('.menu-card');
+
+    // Loop melalui setiap menu card
+    menuCards.forEach(function(card) {
+        var title = card.querySelector('.menu-card-title').innerText.toUpperCase();
+        // Memeriksa apakah judul menu cocok dengan input pencarian
+        if (title.indexOf(input) > -1) {
+            card.style.display = '';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+}
